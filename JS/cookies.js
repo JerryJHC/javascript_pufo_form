93ff75908f3b4ac756d9103c8e17ed104110570e
Jerry Henríquez <jerryhenriquez@gmail.com>
2019-01-08 18:39:58 +0100

    agregado ejemplo de get cookie

--- JS/cookies.js
+++ JS/cookies.js
@@ -5,9 +5,31 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
 function save() {
     setCookie('user', document.forms['reg']['user'].value, 10);
     setCookie('pass', document.forms['reg']['pass'].value, 10);
 }
 
+function getUsers(){
+    return getCookie('user');
+}
+
 document.getElementById("save").addEventListener('click', save);
+
+document.getElementById('users').value = getUsers();
